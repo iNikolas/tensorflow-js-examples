5e8c75adbc76a6025aadf9dc3e77daa424321044
Mykola Lebid <realkolos1@gmail.com>
2025-09-08 00:56:27 +0300

    chore/ simple tf example

--- src/pages/home/view.tsx
+++ src/pages/home/view.tsx
@@ -1,3 +1,27 @@
+import * as tf from "@tensorflow/tfjs";
+
+tf.tidy(() => {
+  const a = tf.tensor2d(
+    [
+      [91, 82, 13],
+      [15, 23, 62],
+      [25, 66, 63],
+    ],
+    [3, 3]
+  );
+  a.print();
+  const b = tf.tensor2d(
+    [
+      [1, 23, 83],
+      [33, 12, 5],
+      [7, 23, 61],
+    ],
+    [3, 3]
+  );
+  b.print();
+  a.matMul(b).print();
+});
+
 export default function Home() {
   return <div />;
 }
