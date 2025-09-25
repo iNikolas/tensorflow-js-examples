9bc8ba6a89c76e82565b4609ce16879419ef7d56
Mykola Lebid <realkolos1@gmail.com>
2025-09-25 22:46:48 +0300

    Updates

---
+++ assets/hooks-B9x-fnbw.js
@@ -0,0 +1,1 @@
+import{R as r}from"./index-B0xbVQ0B.js";function u(e){const[c,a]=r.useState("");return r.useEffect(()=>{a(t=>(t&&URL.revokeObjectURL(t),e?URL.createObjectURL(e):""))},[e]),c}export{u};
--- assets/hooks-BNeZ5eqU.js
+++
@@ -1,1 +0,0 @@
-import{R as r}from"./index-kK9qlud8.js";function u(e){const[c,a]=r.useState("");return r.useEffect(()=>{a(t=>(t&&URL.revokeObjectURL(t),e?URL.createObjectURL(e):""))},[e]),c}export{u};
