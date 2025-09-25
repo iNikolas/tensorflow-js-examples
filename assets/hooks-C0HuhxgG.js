adc998b22711bd4f366b4338a5c0d2cec3444878
Mykola Lebid <realkolos1@gmail.com>
2025-09-25 22:19:22 +0300

    Updates

---
+++ assets/hooks-C0HuhxgG.js
@@ -0,0 +1,1 @@
+import{R as r}from"./index-C17uvJyM.js";function u(e){const[c,a]=r.useState("");return r.useEffect(()=>{a(t=>(t&&URL.revokeObjectURL(t),e?URL.createObjectURL(e):""))},[e]),c}export{u};
